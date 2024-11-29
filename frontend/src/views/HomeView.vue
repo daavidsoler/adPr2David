e1ba4a7df87081b9e5ced88203cf4f42f6d608fe
daavidsoler <davidsolerj03@gmail.com>
2024-11-29 18:57:07 +0100

    subir practica

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <h1>{{ msg }}</h1>
+  <input v-model="msg">
+  </main>
+</template>
